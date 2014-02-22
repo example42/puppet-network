eca6b0ff3dcddc38c27bf5639ab04732bd87851e
Riccardo Capecchi <r.capecchi@cineca.it>
2014-02-22 16:00:46 +0100

    Changed the network::route define to work also on Debian systems

---
+++ manifests/route.pp
@@ -0,0 +1,101 @@
+# == Definition: network::rhel_route
+#
+# Based on https://github.com/razorsedge/puppet-network/ route.pp manifest.
+# Configures /etc/sysconfig/networking-scripts/route-$name on Rhel
+# Add 2 files on Debian 1 under /etc/network/if-up.d and 1 in /etc/network/if-down.d
+#
+# === Parameters:
+#
+#   $ipaddress - required
+#   $netmask   - required
+#   $gateway   - required
+#
+# [*config_file_notify*]
+#   String. Optional. Default: 'class_default'
+#   Defines the notify argument of the created file.
+#   The default special value implies the same behaviour of the main class
+#   configuration file. Set to undef to remove any notify, or set
+#   the name(s) of the resources to notify
+#
+#
+# === Actions:
+#
+# On Rhel
+# Deploys the file /etc/sysconfig/network-scripts/route-$name.
+#
+# On Debian
+# Deploy 2 files 1 under /etc/network/if-up.d and 1 in /etc/network/if-down.d
+#
+# === Sample Usage:
+#
+#   network::route { 'eth0':
+#     ipaddress => [ '192.168.17.0', ],
+#     netmask   => [ '255.255.255.0', ],
+#     gateway   => [ '192.168.17.250', ],
+#   }
+#
+#   network::route { 'bond2':
+#     ipaddress => [ '192.168.2.0', '10.0.0.0', ],
+#     netmask   => [ '255.255.255.0', '255.0.0.0', ],
+#     gateway   => [ '192.168.1.1', '10.0.0.1', ],
+#   }
+#
+# === Authors:
+#
+# Mike Arnold <mike@razorsedge.org>
+# Riccardo Capecchi <riccio.cri@gmail.com>
+#
+# === Copyright:
+#
+# Copyright (C) 2011 Mike Arnold, unless otherwise noted.
+#
+define network::route (
+  $ipaddress,
+  $netmask,
+  $gateway
+) {
+  # Validate our arrays
+  validate_array($ipaddress)
+  validate_array($netmask)
+  validate_array($gateway)
+
+  include 'network'
+
+  $interface = $name
+
+  case $::osfamily {
+    RedHat: {
+      file { "route-${interface}":
+        ensure      => 'present',
+        mode        => '0644',
+        owner       => 'root',
+        group       => 'root',
+        path        => "/etc/sysconfig/network-scripts/route-${interface}",
+        content     => template('network/route-RedHat.erb'),
+        notify      => $network::manage_config_file_notify,
+      }
+    }
+    Debian: {
+      file { "routeup-${interface}":
+        ensure      => 'present',
+        mode        => '0644',
+        owner       => 'root',
+        group       => 'root',
+        path        => "/etc/network/if-up.d/z90-route-${interface}",
+        content     => template('network/route_up-Debian.erb'),
+        notify      => $network::manage_config_file_notify,
+      }
+      file { "routedown-${interface}":
+        ensure      => 'present',
+        mode        => '0644',
+        owner       => 'root',
+        group       => 'root',
+        path        => "/etc/network/if-down.d/z90-route-${interface}",
+        content     => template('network/route_down-Debian.erb'),
+        notify      => $network::manage_config_file_notify,
+      }
+
+    }
+    default: { fail('Operating system not supported')  }
+  }
+} # define network::route
