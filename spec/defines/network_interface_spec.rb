1832443aaf420cade3dcbc81d74814aca6d9f7fa
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2016-12-21 19:45:22 +0100

    Allow passing multiple secondary IP addresses on Redhat
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- spec/defines/network_interface_spec.rb
+++ spec/defines/network_interface_spec.rb
@@ -27,4 +27,32 @@ describe 'network::interface' do
 
   end
 
+  context 'Test network:interface on RedHat with multiple IPs' do
+    let(:title) { 'eth0' }
+    let(:node) { 'rspec.example42.com' }
+    let(:facts) { { :architecture => 'x86_64', :osfamily => 'RedHat', :operatingsystem => 'RedHat' } }
+    let(:params) {
+      { 'enable'       =>  true,
+        'ipaddress'    =>  ['192.168.0.1','192.168.0.2'],
+      }
+    }
+
+    it {
+      is_expected.to contain_file('/etc/sysconfig/network-scripts/ifcfg-eth0').with_ensure('present')
+    }
+
+    it {
+      is_expected.to contain_file('/etc/sysconfig/network-scripts/ifcfg-eth0').with_content(/IPADDR1=\"192.168.0.1\"/)
+    }
+
+    it {
+      is_expected.to contain_file('/etc/sysconfig/network-scripts/ifcfg-eth0').with_content(/IPADDR2=\"192.168.0.2\"/)
+    }
+
+    it {
+      is_expected.to contain_file('/etc/sysconfig/network-scripts/ifcfg-eth0').with_content(/ONBOOT=\"yes\"/)
+    }
+
+  end
+
 end
