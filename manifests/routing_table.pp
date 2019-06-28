3a4ae131b5c6d67051e041d97174125e66cabe8e
Alessandro Franceschi <al@example42.com>
2019-06-28 18:04:10 +0200

    New network::routing_table define based on legacy network::routing_table

--- manifests/routing_table.pp
+++ manifests/routing_table.pp
@@ -1,9 +1,46 @@
-# A description of what this defined type does
+# == Definition: network::routing_table
 #
-# @summary A short summary of the purpose of this defined type.
+# Configures /etc/iproute2/rt_tables
 #
-# @example
-#   network::routing_table { 'namevar': }
-define network::routing_table(
-) {
-}
+# === Parameters:
+#
+#   $table_id - required
+#
+# === Actions:
+#
+# Adds routing table id and name to /etc/iproute2/rt_tables
+#
+# === Sample Usage:
+#
+#   network::routing_table { 'vlan22':
+#     table_id => '200',
+#   }
+#
+# === Authors:
+#
+# Marcus Furlong <furlongm@gmail.com>
+#
+
+define network::routing_table (
+  String $table_id,
+  String $table = $name
+  ) {
+
+  if ! defined(Concat['/etc/iproute2/rt_tables']) {
+    concat { '/etc/iproute2/rt_tables':
+      owner => 'root',
+      group => 'root',
+      mode  => '0644',
+    }
+
+    concat::fragment { 'rt_tables-base':
+      target => '/etc/iproute2/rt_tables',
+      source => 'puppet:///modules/network/legacy/rt_tables',
+    }
+  }
+
+  concat::fragment { "rt_tables-${table}":
+    target  => '/etc/iproute2/rt_tables',
+    content => "${table_id}\t${table}\n",
+  }
+} # define network::routing_table
